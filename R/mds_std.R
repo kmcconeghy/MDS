2ad514d41364ce67111590295b170f05dd0480c0
kmcconeghy <kevinmcconeghy@gmail.com>
2018-01-10 11:29:22 -0500

    edited mds_std files

--- R/mds_std.R
+++ R/mds_std.R
@@ -2,15 +2,15 @@
 #'
 #' Standard MDS data dictionary; includes list of variables and attributes. If
 #' not specified otherwise, all tests / format checks will use this file for
-#' comparing.
+#' comparison.
 #'
-#' @format A data frame with 346342 rows and 8 variables:
+#' @format A data frame with XX rows and 5 variables:
 #' \describe{
-#'   \item{var}{Variable Name}
-#'   \item{label}{Variable Label}
-#'   \item{desc}{Variable Description}
-#'   \item{type}{Variable type}
-#'   \item{length}{Variable length}
+#'   \item{Var}{Variable Name}
+#'   \item{Label}{Variable Label}
+#'   \item{Type}{Variable type}
+#'   \item{Length}{Variable length}
+#'   \item{Format}{Variable Special Format, e.g. Date}
+#'   \item{Description}{Variable Description}
 #' }
-#' @source \url{https://data.cdc.gov/dataset/Deaths-in-122-U-S-cities-1962-2016-122-Cities-Mort/mr8w-325u}
 "mds_std"
--- data-raw/mds_rawtodata.R
+++ data-raw/mds_rawtodata.R
@@ -1,13 +1,24 @@
+library(tidyverse)
+
+# Standard mds variable list into .Rda file
+  #Input
+    mds_stdpull <- readr::read_tsv('mds_stdpull.csv', col_names=F) %>%
+    rename(Var = X1)
+
 # File to convert raw mds_varlist into .Rda file
 
-#Set to data-raw wd
-  setwd("~/GitHub/MDS/data-raw")
+  #Set to data-raw wd
+    setwd("~/GitHub/MDS/data-raw")
 
-#Input
-  mds_varlist <- readr::read_csv('mds_varlist.csv')
+  #Input
+    mds_varlist <- readxl::read_xlsx('mds_vardict.xlsx')
 
-#Format
-  mds_std <- mds_varlist
+  #Format
+    mds_std <- mds_varlist %>%
+      select(Var, Label, Type, Length, Format, Description) %>%
+      inner_join(., mds_stdpull, by=c('Var'))
 
-#Standard
-  devtools::use_data(mds_std, overwrite=T)
+  #Standard
+    devtools::use_data(mds_std, overwrite=T)
+
+
