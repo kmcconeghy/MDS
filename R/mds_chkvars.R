ba9d47530cda4abb9408bed668af130fcddd4c7c
kmcconeghy <kevinmcconeghy@gmail.com>
2018-06-27 13:33:35 -0400

    new functions

---
+++ R/mds_chkvars.R
@@ -0,0 +1,38 @@
+chk_bene_id_18900 <- function(x) {
+  testthat::expect_is(x, character)
+  
+  
+}
+
+chk_DMREPID <- function(x) {
+  testthat::expect_is(x, character)
+  
+  
+}
+
+chk_DMRECID <- function(x) {
+  testthat::expect_is(x, character)
+  
+  
+}
+
+chk_DMRECID <- function(x) {
+  testthat::expect_is(x, character)
+  
+  
+}
+
+chk_dmdate <- function(x) {
+  testthat::expect_is(x, date)
+  
+}
+
+chk_dmadmit <- function(x) {
+  testthat::expect_is(x, date)
+  
+}
+
+chk_dmtype <- function(x) {
+  testthat::expect_is(x, numeric)
+  
+}
