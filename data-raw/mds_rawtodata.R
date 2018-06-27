13a217e519726e27d68534cb416531e5e12cd281
kmcconeghy <kevinmcconeghy@gmail.com>
2018-06-27 13:34:01 -0400

    dictionary edits

--- data-raw/mds_rawtodata.R
+++ data-raw/mds_rawtodata.R
@@ -1,24 +1,15 @@
 library(tidyverse)
+#Set to data-raw wd
+setwd("~/GitHub/MDS/data-raw")
 
 # Standard mds variable list into .Rda file
   #Input
-    mds_stdpull <- readr::read_tsv('mds_stdpull.csv', col_names=F) %>%
-    rename(Var = X1)
 
 # File to convert raw mds_varlist into .Rda file
-
-  #Set to data-raw wd
-    setwd("~/GitHub/MDS/data-raw")
-
   #Input
-    mds_varlist <- readxl::read_xlsx('mds_vardict.xlsx')
-
-  #Format
-    mds_std <- mds_varlist %>%
-      select(Var, Label, Type, Length, Format, Description) %>%
-      inner_join(., mds_stdpull, by=c('Var'))
+    mds_varlist <- readxl::read_xlsx('mdsR_dict.xlsx')
 
   #Standard
-    devtools::use_data(mds_std, overwrite=T)
+    devtools::use_data(mds_varlist, overwrite=T)
 
 
