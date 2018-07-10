ad0ac66f5940d3dc106e7cfb3d7b42772b376128
kmcconeghy <kevinmcconeghy@gmail.com>
2018-07-10 13:43:31 -0400

    edited rpt_xls file

--- R/mds_rpt_xls.R
+++ R/mds_rpt_xls.R
@@ -20,15 +20,20 @@
 #' mds_rpt_xls(mds_dta, 'mds.xslx')
 #'
 #' @references
-mds_rpt_xls <- function(mds_object,
+mds_rpt_xls <- function(mds_obj,
                         filepath,
                         quietly = F) {
 
+  # Must be dataframe
+  stopifnot(any(class(mds_obj)=='data.frame'))
+
+  #Must have certain columns
+  std_columns <- c('bene_id_18900', 'dmdate', 'DMRECID')
+  stopifnot(std_columns %in% names(mds_obj))
 
   ## Set-up file
-  mds_rpt_style()
-
-  obj_name_eq <- rlang::enquo(mds_object)
+  xl_style <- mds_rpt_sty()
+  obj_name <- rlang::quo(mds_obj)
 
   ## Set-up workbook
   xlsx.wb <- openxlsx::createWorkbook()
@@ -38,10 +43,11 @@ mds_rpt_xls <- function(mds_object,
 
   openxlsx::addWorksheet(xlsx.wb, sheetName = 'MDS File')
 
+
   ## Title Box
   if (quietly==F) {
     cat('General Report on Minimum Dataset File \n')
-    cat('R gloal environ. object: ', paste0(obj_name_eq[2]), '\n')
+    cat('R gloal environ. object: ', rlang::quo_text(obj_name), '\n')
     cat('Date Created: ', Scotty::TimeStamp(), 'Programmer: ', 'KWM',  '\n')
   }
 
@@ -49,23 +55,44 @@ mds_rpt_xls <- function(mds_object,
   openxlsx::writeData(xlsx.wb, 1,
                       'General Report on Minimum Dataset File',
                       startRow=1, startCol=1)
-  openxlsx::addStyle(xlsx.wb, 1, style=title_style, rows=1, cols=1)
+  openxlsx::addStyle(xlsx.wb, 1, style=xl_style$title, rows=1, cols=1)
 
   ## Subheading
   openxlsx::writeData(xlsx.wb, 1,
-                      paste0('R gloal environ. object: ', obj_name_eq[2]),
+                      paste0('R gloal environ. object: ', rlang::quo_text(obj_name)),
                       startRow=2, startCol=1)
-  openxlsx::addStyle(xlsx.wb, 1, style=subhead_style, rows=2, cols=1)
 
   openxlsx::writeData(xlsx.wb, 1, paste0('Date Created: ',
                                          Scotty::TimeStamp(),
                                          ', Programmer: ',
                                          'KWM'),
                       startRow=3, startCol=1)
-  openxlsx::addStyle(xlsx.wb, 1, style=subhead_style, rows=3, cols=1)
+  openxlsx::addStyle(xlsx.wb, 1, style=xl_style$subhead, rows=2:3, cols=1)
 
+  ## Contents
+  res <- mds_rpt_res(mds_obj)
 
+  openxlsx::writeData(xlsx.wb, 1,
+                      paste0('Observations: ', formatC(res$nrows, big.mark = ',')),
+                      startRow=6, startCol=1)
+  openxlsx::writeData(xlsx.wb, 1,
+                      paste0('Columns: ', formatC(res$ncols, big.mark = ',')),
+                      startRow=7, startCol=1)
+  openxlsx::writeData(xlsx.wb, 1,
+                      paste0('First assesment: ', res$first_asmt,
+                             ', Late assessment: ', res$last_asmt),
+                      startRow=8, startCol=1)
+  openxlsx::writeData(xlsx.wb, 1,
+                      paste0('No. distinct patients: ', formatC(res$n_patients, big.mark = ',')),
+                      startRow=9, startCol=1)
+  openxlsx::writeData(xlsx.wb, 1,
+                      paste0('Assessment per patient: ', res$asmt_perpat),
+                      startRow=10, startCol=1)
+  openxlsx::writeData(xlsx.wb, 1,
+                      paste0('Duplicates by DMRECID: ', res$n_duprecs),
+                      startRow=11, startCol=1)
 
+  openxlsx::addStyle(xlsx.wb, 1, style=xl_style$result, rows=6:11, cols=1)
 
   ## Save final workbook
   openxlsx::saveWorkbook(xlsx.wb, file=filepath, overwrite = T)
