c041cab5ee6f3401820df3fba8ca1608e7e94b9c
kmcconeghy <kevinmcconeghy@gmail.com>
2018-07-10 13:52:42 -0400

    pgk_down, vignettes

---
+++ vignettes/03-chkmds.R
@@ -0,0 +1,6 @@
+## ---- echo = FALSE, message = FALSE--------------------------------------
+knitr::opts_chunk$set(collapse = T, comment = "#>")
+options(tibble.print_min = 4L, tibble.print_max = 4L)
+library(mdsR)
+library(tidyverse)
+
